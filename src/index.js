41099b5ab62de38262756231e611e20255ce4ba1
Chris <chris@uber5.com>
2019-10-14 15:36:31 +0200

    Added libs react-scripts, react, babel, etc

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Comp1 } from './lib'
+
+function App() {
+  return <Comp1 />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const Comp1 = ({ bla }) => <p>Comp1, bla={bla || '(not set)'}</p>
