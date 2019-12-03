ef32c70cc8365a776fcb9e427c60b0b4230807b8
Michael Moldoveanu <moldy530@gmail.com>
2019-12-02 19:18:00 -0500

    fix: each render creates a new instance

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,27 +1,31 @@
-import React, { useRef, useState, useEffect } from 'react'
-import * as rv from 'rough-viz/dist/roughviz.min'
+import React, { useRef, useState, useEffect } from 'react';
+import * as rv from 'rough-viz/dist/roughviz.min';
 
-const generateId = prefix => `${prefix}${('' + Math.random()).split('.')[1]}`
+const generateId = prefix => `${prefix}${('' + Math.random()).split('.')[1]}`;
 
 const wrap = rvComp => ({ prefix, ...props }) => {
-  const ref = useRef()
-  const [ id ] = useState(generateId(prefix || 'roughviz-'))
+  const ref = useRef();
+  const [id] = useState(generateId(prefix || 'roughviz-'));
   useEffect(() => {
     if (ref.current) {
+      // since this effect runs only on prop changes,
+      // it's safe to assume we want to redraw
+      ref.current.childNodes.forEach(node => node.remove());
+
       new rvComp({
         element: '#' + id,
         ...props
-      })
+      });
     }
-  }, [ ref, id, props ])
+  }, [id, props]);
 
-  return <div id={id} ref={ref} />
-}
+  return <div id={id} ref={ref} />;
+};
 
-export const Bar = wrap(rv.Bar)
-export const BarH = wrap(rv.BarH)
-export const Donut = wrap(rv.Donut)
-export const Pie = wrap(rv.Pie)
-export const Scatter = wrap(rv.Scatter)
-export const Line = wrap(rv.Line)
-export const StackedBar = wrap(rv.StackedBar)
+export const Bar = wrap(rv.Bar);
+export const BarH = wrap(rv.BarH);
+export const Donut = wrap(rv.Donut);
+export const Pie = wrap(rv.Pie);
+export const Scatter = wrap(rv.Scatter);
+export const Line = wrap(rv.Line);
+export const StackedBar = wrap(rv.StackedBar);
